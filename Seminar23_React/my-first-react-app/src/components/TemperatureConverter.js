005577389ea2e2fbb1f1481b7eb040658ec834b5
Anna Sisenina <yuta4ka71@gmail.com>
2024-06-21 18:23:03 +0300

    bug in TemperatureConverter was fixed

--- Seminar23_React/my-first-react-app/src/components/TemperatureConverter.js
+++ Seminar23_React/my-first-react-app/src/components/TemperatureConverter.js
@@ -1,36 +1,66 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function TemperatureConverter() {
-    const [celsius, setCelsius] = useState(0);
+  const [celsius, setCelsius] = useState("");
 
-    const convertToCelcius = (event) => {
-        const value = parseFloat(event.target.value);
-        setFahrenheit(value);
-        setCelsius((value - 32) * 5 / 9);
+  const convertToCelcius = (event) => {
+    const value = event.target.value;
+    if (value === "-" || value === "") {
+      setFahrenheit(value);
+      setCelsius("");
     }
-
-    const [fahrenheit, setFahrenheit] = useState(32);
-
-    const convertToFahrenheit = (event) => {
-        const value = parseFloat(event.target.value);
-        setCelsius(value);
-        setFahrenheit((value * 9 / 5) + 32);
+    else if (!isNaN(value)) {
+      setFahrenheit(value);
+      setCelsius(((value - 32) * 5) / 9);
+    } else {
+      setFahrenheit("");
+      setCelsius("");
     }
+  };
+
+  const [fahrenheit, setFahrenheit] = useState("");
+
+  const convertToFahrenheit = (event) => {
+    const value = event.target.value;
+    if (value === "-" || value === "") {
+      setCelsius(value);
+      setFahrenheit("");
+    }
+    else if (!isNaN(value)) {
+      setCelsius(value);
+      setFahrenheit((value * 9) / 5 + 32);
+    } else {
+      setFahrenheit("");
+      setCelsius("");
+    }
+  };
 
   return (
-    <Box 
+    <Box
       component="form"
       sx={{
-        '& > :not(style)': { m: 1, width: '15ch' },
+        "& > :not(style)": { m: 1, width: "15ch" },
       }}
       noValidate
       autoComplete="off"
-      style={{display:"flex", justifyContent: "flex-end"}}
+      style={{ display: "flex", justifyContent: "flex-end" }}
     >
-      <TextField id="standard-basic" label="°C" variant="standard" value={celsius} type="number" onChange={convertToFahrenheit} />
-      <TextField id="standard-basic" label="℉" variant="standard" value={fahrenheit} type="number" onChange={convertToCelcius}/>
+      <TextField
+        id="standard-basic"
+        label="°C"
+        variant="standard"
+        value={celsius}
+        onChange={convertToFahrenheit}
+      />
+      <TextField
+        id="standard-basic"
+        label="℉"
+        variant="standard"
+        value={fahrenheit}
+        onInput={convertToCelcius}
+      />
     </Box>
   );
 }
